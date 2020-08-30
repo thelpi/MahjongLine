1abc11f0f36c6a9eb8bde4c6327013d265e08142
Lpi <thelpi.v3@gmail.com>
2020-08-30 14:25:38 +0200

    Unit tests project from client

---
+++ MahjongLineClientUnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MahjongLineClientUnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MahjongLineClientUnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("1559adcf-5a76-4ba3-83d4-b3b7595722db")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ MahjongLineClientUnitTests/UnitTest1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+
+namespace MahjongLineClientUnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            ServerDto dto1 = new ServerDto { Aaaah = "Babar" };
+            ServerDto dto2 = new ServerDto { Aaaah = "Toto" };
+
+            List<Tuple<ServerDto, bool>> serverTuples =
+                new List<Tuple<ServerDto, bool>>
+                {
+                    new Tuple<ServerDto, bool>(dto1, true),
+                    new Tuple<ServerDto, bool>(dto2, false),
+                };
+
+            string stringTuples = JsonConvert.SerializeObject(serverTuples);
+
+            var clientTuples = JsonConvert.DeserializeObject<List<Tuple<ClientDto, bool>>>(stringTuples);
+        }
+    }
+
+    public class ServerDto
+    {
+        public string Aaaah { get; set; }
+    }
+
+    public class ClientDto
+    {
+        public string Aaaah { get; set; }
+    }
+}
